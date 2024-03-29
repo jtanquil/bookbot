73f927849e98b9a34f3df739331e4c304878f0e6
jtanquil <jose.tanquilut@gmail.com>
2024-03-28 22:14:30 -0700

    Added character count

--- main.py
+++ main.py
@@ -2,8 +2,27 @@ def get_book(filepath):
   with open(filepath) as f:
     return f.read()
 
+def get_words(book):
+  return book.split()
+
 def get_word_count(book):
-  return len(book.split())
+  return len(get_words(book))
+
+# given a word list
+# for each word, split up the word into letters
+# for each letter, add it to the dictionary (if not there), or increment it
+def get_char_count(book):
+  char_count = {}
+  word_list = get_words(book)
+
+  for word in word_list:
+    for char in word.lower():
+      if char in char_count:
+        char_count[char] += 1
+      else:
+        char_count[char] = 1
+
+  return char_count
 
 def main():
   path = "books/frankenstein.txt"
@@ -11,5 +30,9 @@ def main():
   word_count = get_word_count(book)
 
   print(f"This book has {word_count} words.")
+
+  char_count = get_char_count(book)
+
+  print(char_count)
 
 main()
